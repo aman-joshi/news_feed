069e63e29c918dcccaf7f01c30d1f9e4d6270063
Aman Joshi <aman.joshi@crownstack.com>
2021-05-05 12:37:34 +0530

    Initial Commit

---
+++ NewFeed/NewFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewFeedApp.swift
+//  NewFeed
+//
+//  Created by Mohd Maruf on 05/05/21.
+//
+
+import SwiftUI
+
+@main
+struct NewFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
